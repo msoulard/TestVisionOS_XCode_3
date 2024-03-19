4e1d1551a2e5dc4fa16203449b49763969aae1bd
Maëva Soulard <58474758+msoulard@users.noreply.github.com>
2024-03-19 16:23:02 +0100

    wip

--- TestVisionOS_XCode_3/ImmersiveView.swift
+++ TestVisionOS_XCode_3/ImmersiveView.swift
@@ -11,7 +11,7 @@ struct ImmersiveView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-    /*func AddCube() -> Entity {
+    /*func AddCube() -> ModelEntity {
         let entity = Entity()
         return entity
     }*/
