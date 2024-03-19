27efd919316d76104440b0f6afb133c9630a4db7
Maëva Soulard <58474758+msoulard@users.noreply.github.com>
2024-03-19 09:55:48 +0100

    Initial Commit

---
+++ TestVisionOS_XCode_3/TestVisionOS_XCode_3App.swift
@@ -0,0 +1,17 @@
+//
+//  TestVisionOS_XCode_3App.swift
+//  TestVisionOS_XCode_3
+//
+//  Created by Maëva Soulard on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestVisionOS_XCode_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
